ab37ba890857c636604c8330c178455cc5461de0
Erik Jung <ErikJung@Eriks-MacBook-Pro.local>
2014-07-18 15:33:42 -0700

    Initial Commit

---
+++ Iron BirdTests/Iron_BirdTests.m
@@ -0,0 +1,34 @@
+//
+//  Iron_BirdTests.m
+//  Iron BirdTests
+//
+//  Created by Erik Jung on 7/18/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Iron_BirdTests : XCTestCase
+
+@end
+
+@implementation Iron_BirdTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
